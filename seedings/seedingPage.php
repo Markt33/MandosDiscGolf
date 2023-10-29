d43a20bc8cdf7cb55be6e85715e8ca02c93f02a0
Mark Tsvyan <marktsvyan24@gmail.com>
2023-10-29 02:17:39 -0400

    reverted some styles

--- seedings/seedingPage.php
+++ seedings/seedingPage.php
@@ -17,6 +17,8 @@ require_once '../includes/admin_check.php';
         crossorigin="anonymous"
     />
     <link rel="stylesheet" href="../styles/style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -136,7 +138,6 @@ require_once '../includes/admin_check.php';
     </br>
 </article>
 
-<!-- footer -->
 
 <footer class="w-100 bg-dark">
     </br>
