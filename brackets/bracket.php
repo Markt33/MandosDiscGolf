9e4fc2a1cad56f0dd881a8431518106794680136
Mark Tsvyan <marktsvyan24@gmail.com>
2023-12-11 00:54:27 -0500

    Configured routing and Nav Bar

--- brackets/bracket.php
+++ brackets/bracket.php
@@ -694,9 +694,9 @@ if ($size == 16) {
             <a class="flex-sm-fill text-sm-center nav-link alink" href="../index.php">Home</a>
             <?php
             if (isset($_SESSION['username'])) {
-                echo '<a class="flex-sm-fill text-sm-center nav-link alink" href="../player_search.php">Player Search</a>';
-                echo '<a class="flex-sm-fill text-sm-center nav-link alink" href="../player_profile.php">Player Profile</a>';
-                echo '<a class="flex-sm-fill text-sm-center nav-link alink" href="../seedings/seedings.php">Seedings</a>';
+                //echo '<a class="flex-sm-fill text-sm-center nav-link alink" href="../player_search.php">Player Search</a>';
+                //echo '<a class="flex-sm-fill text-sm-center nav-link alink" href="../player_profile.php">Player Profile</a>';
+                echo '<a class="flex-sm-fill text-sm-center nav-link alink" href="../seedings/seedingPage.php">Seedings</a>';
             }
             ?>
             <a class="flex-sm-fill text-sm-center nav-link alink new-active" href="brackets.php">Brackets</a>
